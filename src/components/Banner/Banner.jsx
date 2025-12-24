add7dcb4856e299ce4e1c31974bb30f272b94572
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-24 19:04:51 +0600

    9th commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,18 +3,18 @@ import BookImg from "../../assets/books.jpg";
 
 const Banner = () => {
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row">
+    <div className="hero bg-gray-300 min-h-600px rounded-3xl my-10">
+      <div className="hero-content flex-col lg:flex-row center">
        
-        <div>
+        <div className="m-10">
           
-          <p className="py-6 font-extrabold text-6xl">
-            Books to freshen up <br /> your bookshelf
+          <p className="py-6 font-extrabold text-4xl text-black">
+            Books to freshen up  your bookshelf
           </p>
-          <button className="btn btn-primary">View The List</button>
+          <button className="btn btn-primary ">View The List</button>
 
         </div> 
-        <img src={BookImg className="max"
+        <img className="w-auto size-80 rounded-4xl" src={BookImg }
         />
       </div>
     </div>
