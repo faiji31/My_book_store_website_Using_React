133479e48da50073df39fce1ba4e534f783ee020
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-24 10:14:25 +0600

    4th commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+      return (
+            <div>
+                  <h3>Hy i am footer</h3>
+            </div>
+      );
+};
+
+export default Footer;
---
+++ src/components/Header/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+      return (
+            <div>
+                  <h3>Hy i Am header</h3>
+            </div>
+      );
+};
+
+export default NavBar;
