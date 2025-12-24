e1719762626c293792966ebbc27a4c7d0fe91b53
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-24 11:55:12 +0600

    5th commit

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from '../pages/Root/Root';
+import Error from '../pages/Error/Error';
+import Home from '../pages/Home/Home';
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component:Root,
+    errorElement:<Error></Error>,
+    children:[
+      {
+            index:true,
+            path:'/',
+            Component:Home
+      },
+    ]
   },
 ]);
